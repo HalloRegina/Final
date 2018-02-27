4ad2f733fba8b3eaac283ebfb1bc43c8a21e506b
HalloRegina <36815865+HalloRegina@users.noreply.github.com>
2018-02-27 18:47:46 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body style="background-color:lavender;">
+        <a href="index.php">Startseite</a>
+        <br>
+        <a href="allMovies.php">Übersicht</a>
+        <br>
+        <a href="suche.php">Suche</a>
+        <br>
+    </body>
+</html>
